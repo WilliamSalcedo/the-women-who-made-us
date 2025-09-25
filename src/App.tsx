062fc7498a9dacbc2a90ebd94adbaf6cd5917159
WilliamSalcedo <wsl_1994@hotmail.com>
2025-09-25 17:49:41 -0500

    fix se formateo codigo con normas de prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css'
 
 function App() {
-  
-
   return (
     <>
-      <p className='text-black-100 font-bold text-3xl'>hola</p>
+      <p className='text-black-100 text-3xl font-bold'>hola</p>
     </>
   )
 }
