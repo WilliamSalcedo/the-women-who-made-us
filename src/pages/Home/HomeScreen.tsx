cfc83c9f3bc58c9f35fc706af1e574996506f048
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-09-26 18:40:09 -0500

    Fix size in margin home

--- src/pages/Home/HomeScreen.tsx
+++ src/pages/Home/HomeScreen.tsx
@@ -1,11 +1,11 @@
 import Header from '../../components/layout/Header'
-import VideoPlay from '../../components/VideoPlay'
+import Main from '../../components/layout/Main'
 
 export default function Home() {
   return (
-    <section className="mx-4 my-6 sm:mx-8 md:mx-20 lg:mx-40 xl:mx-60">
+    <section className="mx-[10px] sm:mx-[20px] lg:mx-[30px] my-6">
       <Header />
-      <VideoPlay />
+      <Main />
     </section>
   )
 }
