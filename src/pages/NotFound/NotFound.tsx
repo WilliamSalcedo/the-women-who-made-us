6edae99de4c4a551a9b475b80def7ab121fcbb28
WilliamSalcedo <wsl_1994@hotmail.com>
2025-09-26 17:56:02 -0500

    se crea componente not found

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 export default function NotFound() {
   return (
     <section className="space-y-4">
       <h1 className="text-2xl font-semibold">404</h1>
       <p className="text-gray-600">Ruta no encontrada.</p>
-      <Link to="/" className="underline">Volver al inicio</Link>
+      <Link to="/" className="underline">
+        Volver al inicio
+      </Link>
     </section>
-  );
+  )
 }
