cbd310ebe9b78f5b03fadd107984306b4330a4c6
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-09-26 18:46:37 -0500

    Fix size, width and height

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,11 +5,11 @@ import tiktok from '../../assets/tiktok-1.svg'
 export default function Header() {
   console.log('aca estoy')
   return (
-    <header className="bg-red w-full">
-      <div className="mx-auto max-w-6xl px-4 py-3">
+    <header className="w-full mb-8">
+      <div className="mx-auto">
         <div className="flex flex-col items-center gap-3 md:flex-row md:justify-between">
           <div className="flex justify-center md:justify-start">
-            <img src={logo} alt="The Women Who Made Us" className="w-28 md:w-40 lg:w-80" />
+            <img src={logo} alt="The Women Who Made Us" className="w-28 md:w-20 lg:w-40" />
           </div>
           <div className="flex items-center justify-center gap-4 md:justify-end">
             <a
@@ -23,12 +23,12 @@ export default function Header() {
               <img
                 src={instagram}
                 alt="Instagram"
-                className="h-6 w-6 transition hover:opacity-75 md:h-8 md:w-8 lg:h-10 lg:w-10"
+                className="h-3 w-3 transition hover:opacity-75 md:h-4 md:w-4 lg:h-6 lg:w-6"
               />
             </a>
 
             <a
-              href="https://www.tiktok.com/@thewomenwhomadeus?_t=ZS-903mQwFausE&_r=1" /* ← pon tu URL real */
+              href="https://www.tiktok.com/@thewomenwhomadeus?_t=ZS-903mQwFausE&_r=1" 
               target="_blank"
               rel="noopener noreferrer"
               className="inline-flex"
@@ -38,7 +38,7 @@ export default function Header() {
               <img
                 src={tiktok}
                 alt="TikTok"
-                className="h-6 w-6 transition hover:opacity-75 md:h-8 md:w-8 lg:h-10 lg:w-10"
+                className="h-3 w-3 transition hover:opacity-75 md:h-4 md:w-4 lg:h-6 lg:w-6"
               />
             </a>
           </div>
