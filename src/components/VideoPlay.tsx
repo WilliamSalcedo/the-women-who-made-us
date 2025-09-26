761c6847b97b3866de49b53402f907bb741b05d7
WilliamSalcedo <wsl_1994@hotmail.com>
2025-09-26 17:55:11 -0500

    se crea componente de video

---
+++ src/components/VideoPlay.tsx
@@ -0,0 +1,9 @@
+import video from '../assets/phantomOpera.avif'
+
+export default function VideoPlay() {
+  return (
+    <div className="flex justify-center">
+      <img src={video} alt="" />
+    </div>
+  )
+}
