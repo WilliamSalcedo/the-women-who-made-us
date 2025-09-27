2a37a88075b1a67a40ee1b167769269e843acaa1
WilliamSalcedo <wsl_1994@hotmail.com>
2025-09-27 17:08:06 -0500

    Se agrega componente de texto

---
+++ src/components/Text.tsx
@@ -0,0 +1,13 @@
+export default function Text() {
+  return (
+    <div className="flex justify-center   ">
+      <p className="text-white text-center sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
+        Live! The Women Who Made Us™ is a bold new theatrical concert celebrating the voices that
+        shaped generations. From powerhouse vocals to unforgettable ballads, this show blends the
+        thrill of a world tour with the intimacy of live theatre. <br /> We are creating something truly
+        special for you. A celebration of icons, stories, and music that will stay with you long
+        after the curtain falls.
+      </p>
+    </div>
+  )
+}
