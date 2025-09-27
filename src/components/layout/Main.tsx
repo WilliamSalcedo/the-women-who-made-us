11d267f3aa6d1794fe90e8b9e643a53383f3f063
WilliamSalcedo <wsl_1994@hotmail.com>
2025-09-27 17:10:49 -0500

    Se agrega componente titulo y texto

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,9 +1,13 @@
+import Text from '../Text'
+import Title from '../Title'
 import VideoPlay from '../VideoPlay'
 
 function Main() {
   return (
     <div>
         <VideoPlay />
+        <Title />
+        <Text />
     </div>
   )
 }
