f4dd88cf85794f88c56ed3d8a3dd6b28e1e8cd0a
WilliamSalcedo <wsl_1994@hotmail.com>
2025-09-27 17:08:24 -0500

    Se agrega componente del titulo

---
+++ src/components/Title.tsx
@@ -0,0 +1,16 @@
+export default function Title() {
+  return (
+    <div>
+      <div className="flex justify-center px-4 pt-10">
+        <h1 className="text-gold font-baskervville max-w-xs text-center text-2xl sm:max-w-md sm:text-3xl md:max-w-2xl md:text-5xl lg:max-w-4xl lg:text-6xl xl:max-w-5xl xl:text-7xl 2xl:max-w-6xl 2xl:text-8xl">
+          A Theatrical Experience Worth Waiting For
+        </h1>
+      </div>
+      <div className="m-4">
+        <p className="text-purple font-montserrat text-center font-semibold tracking-[0.25em] uppercase text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+          PREMIERING SOON
+        </p>
+      </div>
+    </div>
+  )
+}
