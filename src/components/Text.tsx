1ea5d528b64e963b46da414a5b5834cfc4f835b2
WilliamSalcedo <wsl_1994@hotmail.com>
2025-09-28 22:17:43 -0500

    Se agrega texto de formulario

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,13 +1,25 @@
 export default function Text() {
   return (
-    <div className="flex justify-center   ">
-      <p className="text-white text-center sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-        Live! The Women Who Made Us™ is a bold new theatrical concert celebrating the voices that
-        shaped generations. From powerhouse vocals to unforgettable ballads, this show blends the
-        thrill of a world tour with the intimacy of live theatre. <br /> We are creating something truly
-        special for you. A celebration of icons, stories, and music that will stay with you long
-        after the curtain falls.
-      </p>
+    <div>
+      <div className="flex justify-center">
+        <p className="text-center text-xs text-white sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl">
+          Live! The Women Who Made Us™ is a bold new theatrical concert celebrating the voices that
+          shaped generations. From powerhouse vocals to unforgettable ballads, this show blends the
+          thrill of a world tour with the intimacy of live theatre. <br /> We are creating something
+          truly special for you. A celebration of icons, stories, and music that will stay with you
+          long after the curtain falls.
+        </p>
+      </div>
+      <div className="m-4 flex justify-center">
+        <p className="text-gold font-baskervville max-w-xs text-center text-lg sm:max-w-md sm:text-xl md:max-w-2xl md:text-2xl lg:max-w-4xl lg:text-3xl xl:max-w-5xl xl:text-4xl 2xl:max-w-6xl 2xl:text-5xl">
+          Be the First to Hear More
+        </p>
+      </div>
+      <div className="m-4 flex justify-center">
+        <p className="m-4 text-center text-xs text-white sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl">
+            Sign up to learn more about this groundbreaking experience. 
+        </p>
+      </div>
     </div>
   )
 }
