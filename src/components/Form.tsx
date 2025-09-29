256deb5ae21f10cb99799eb31fadae639654f744
WilliamSalcedo <wsl_1994@hotmail.com>
2025-09-29 17:12:35 -0500

    Se remplazo texto por variables de traduccion

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,31 +1,35 @@
+import { useTranslation } from "react-i18next"
+
 export default function Form() {
+const { t }=useTranslation();
+
   return (
     <div className="flex justify-center">
       <form className="m-4 flex w-full max-w-3xl flex-col items-center md:flex-col md:gap-6">
         <div className="flex flex-col gap-4 md:flex-row md:gap-6">
           <input
             type="text"
-            placeholder="First Name"
+            placeholder={t("text.firstName")}
             autoComplete="given-name"
-            className="w-32 rounded-xl border border-white bg-transparent px-2 py-0.5 text-center text-sm text-white placeholder:text-xs focus:ring-2 focus:ring-white/60 focus:outline-none sm:text-base sm:placeholder:text-sm md:w-56 md:px-3 md:py-2 md:text-lg md:placeholder:text-base lg:text-xl lg:placeholder:text-lg"
+            className="w-32 rounded-xl border border-gold bg-transparent px-2 py-0.5 text-center text-sm text-white placeholder:text-xs focus:ring-2 focus:ring-white/60 focus:outline-none sm:text-base sm:placeholder:text-sm md:w-56 md:px-3 md:py-2 md:text-lg md:placeholder:text-base lg:text-xl lg:placeholder:text-lg"
           />
 
           <input
             type="text"
-            placeholder="Last Name"
+            placeholder={t("text.lastName")}
             autoComplete="family-name"
-            className="w-32 rounded-xl border border-white bg-transparent px-2 py-0.5 text-center text-sm text-white placeholder:text-xs focus:ring-2 focus:ring-white/60 focus:outline-none sm:text-base sm:placeholder:text-sm md:w-56 md:px-3 md:py-2 md:text-lg md:placeholder:text-base lg:text-xl lg:placeholder:text-lg"
+            className="w-32 rounded-xl border border-gold bg-transparent px-2 py-0.5 text-center text-sm text-white placeholder:text-xs focus:ring-2 focus:ring-white/60 focus:outline-none sm:text-base sm:placeholder:text-sm md:w-56 md:px-3 md:py-2 md:text-lg md:placeholder:text-base lg:text-xl lg:placeholder:text-lg"
           />
 
           <input
             type="email"
-            placeholder="Email"
+            placeholder={t("text.email")}
             autoComplete="email"
-            className="w-32 rounded-xl border border-white bg-transparent px-2 py-0.5 text-center text-sm text-white placeholder:text-xs focus:ring-2 focus:ring-white/60 focus:outline-none sm:text-base sm:placeholder:text-sm md:w-56 md:px-3 md:py-2 md:text-lg md:placeholder:text-base lg:text-xl lg:placeholder:text-lg"
+            className="w-32 rounded-xl border border-gold bg-transparent px-2 py-0.5 text-center text-sm text-white placeholder:text-xs focus:ring-2 focus:ring-white/60 focus:outline-none sm:text-base sm:placeholder:text-sm md:w-56 md:px-3 md:py-2 md:text-lg md:placeholder:text-base lg:text-xl lg:placeholder:text-lg"
           />
         </div>
-        <button className="bg-gold mt-4 w-20 text-xs md:py-2 md:w-40 md:mt-0.5 md:text-base  rounded-md border border-white text-center font-semibold text-black ">
-          SIGN UP
+        <button className="bg-gold mt-4 w-20 text-xs md:py-2 md:w-40 md:mt-0.5 md:text-base  rounded-md border border-white text-center font-bold text-black ">
+          {t("text.btmRegister")}
         </button>
       </form>
     </div>
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,25 +1,27 @@
+import { useTranslation, Trans } from "react-i18next";
+
 export default function Text() {
+  const { t } = useTranslation();
+
   return (
     <div>
       <div className="flex justify-center">
-        <p className="text-center text-xs text-white sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl">
-          Live! The Women Who Made Us™ is a bold new theatrical concert celebrating the voices that
-          shaped generations. From powerhouse vocals to unforgettable ballads, this show blends the
-          thrill of a world tour with the intimacy of live theatre. <br /> We are creating something
-          truly special for you. A celebration of icons, stories, and music that will stay with you
-          long after the curtain falls.
+        <p className="text-center text-xs text-white sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-futura">
+          <Trans i18nKey="text.intro" />
         </p>
       </div>
+
       <div className="m-4 flex justify-center">
         <p className="text-gold font-baskervville max-w-xs text-center text-lg sm:max-w-md sm:text-xl md:max-w-2xl md:text-2xl lg:max-w-4xl lg:text-3xl xl:max-w-5xl xl:text-4xl 2xl:max-w-6xl 2xl:text-5xl">
-          Be the First to Hear More
+          {t("text.ctaTitle")}
         </p>
       </div>
+
       <div className="m-4 flex justify-center">
-        <p className="m-4 text-center text-xs text-white sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl">
-            Sign up to learn more about this groundbreaking experience. 
+        <p className="m-4 text-center text-xs text-white sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-futura">
+          {t("text.ctaDescription")}
         </p>
       </div>
     </div>
-  )
+  );
 }
