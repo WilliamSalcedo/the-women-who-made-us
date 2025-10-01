1966cd61be620bf3f7a8b59da89a4634a711d3fc
WilliamSalcedo <wsl_1994@hotmail.com>
2025-10-01 16:08:28 -0500

    Se actualiza componente de video

--- src/components/VideoPlay.tsx
+++ src/components/VideoPlay.tsx
@@ -1,9 +1,17 @@
-import video from '../assets/phantomOpera.avif'
-
-export default function VideoPlay() {
+export default function VideoHero() {
+  const YT_ID = 'NUz4d-bd2hs'
+  const src = `https://www.youtube.com/embed/${YT_ID}?autoplay=1&mute=1&loop=1&playlist=${YT_ID}&controls=0&modestbranding=1&rel=0&playsinline=1`
   return (
-    <div className="flex justify-center">
-      <img src={video} alt="" />
+      <div className="flex justify-center py-10">
+      <div className="w-3xl lg:w-7xl aspect-video relative rounded-2xl overflow-hidden">
+        <iframe
+          className="absolute inset-0 w-full h-full"
+          src={src}
+          title="YouTube video"
+          allow="autoplay; encrypted-media; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
     </div>
   )
 }
