67749e01ea82620c8f9698254af332fa6b871d21
WilliamSalcedo <wsl_1994@hotmail.com>
2026-02-01 16:00:01 -0500

    Add logic for modals

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,21 +1,38 @@
-import { useTranslation } from "react-i18next"
-
-
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import PrivacyModal from '../PrivacyModal'
+import TermsModal from '../TermsModal'
 
 export default function Footer() {
-const { t }=useTranslation();
+  const [isActivePolicy, setIsActivePolicy] = useState(false)
+  const [isactiveterms, setIsActiveTerms] = useState(false)
+  const { t } = useTranslation()
+  const onHandlePrivacy = () => {
+    setIsActivePolicy(true)
+  }
+  const onHandleTerms = ()=>{
+    setIsActiveTerms(true)
+  }
+
+  const onClose = () => {
+    setIsActivePolicy(false)
+    setIsActiveTerms(false)
+  }
+  
 
   return (
     <div>
-      <div className="text-white flex justify-center font-futura divide-x divide-white mt-20 text-[9px] md:text-sm lg:text-base">
-        <a href="" className="transition hover:opacity-75 px-3">{t("text.privacy")}</a>
-        <a href="" className="transition hover:opacity-75 pl-3">{t("text.terms")}</a>
+      <div className="font-futura mt-20 flex justify-center divide-x divide-white text-[9px] text-white md:text-sm lg:text-base">
+        <button onClick={onHandlePrivacy} className="px-3 transition hover:opacity-75">
+          {t('text.privacy')}
+        </button>
+        <button onClick={onHandleTerms} className="px-3 transition hover:opacity-75">{t('text.terms')}</button>
       </div>
-      <div className="text-white text-center font-futura m-3 italic text-[10px] md:text-sm lg:text-base">
-        <p>
-            {t("text.legalText")}
-        </p>
+      <div className="font-futura m-3 text-center text-[10px] font-semibold text-white italic md:text-sm lg:text-base">
+        <p>{t('text.legalText')}</p>
       </div>
+      <PrivacyModal isActive={isActivePolicy} onClose={onClose} />
+      <TermsModal isActive={isactiveterms} onClose={onClose} />
     </div>
   )
 }
