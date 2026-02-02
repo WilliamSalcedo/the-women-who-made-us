d6ebe2ca168ccab7cbb0ce06b8363050b2a4c1c3
WilliamSalcedo <wsl_1994@hotmail.com>
2026-02-02 18:06:00 -0500

    update component

--- src/components/TermsModal.tsx
+++ src/components/TermsModal.tsx
@@ -1,28 +1,21 @@
 import { useTranslation } from 'react-i18next'
 
-type ModalProps = {
-  isActive: boolean
-  onClose: () => void
-}
-
-export default function TermsModal({ isActive, onClose }: ModalProps) {
+export default function TermsModal() {
   const { t } = useTranslation()
-
-  if (!isActive) return null
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/70">
       <div className="relative w-[90%] max-w-2xl rounded-lg border border-[#fda963] bg-black p-6 shadow-xl">
-        <button
-          onClick={onClose}
-          className="absolute top-4 right-4 text-white opacity-70 hover:opacity-100"
-        >
-          ✕
-        </button>
-        <h1 className="font-futura mb-2 text-center text-white italic  md:text-sm lg:text-base">
+        <h1 className="font-futura mb-2 text-center text-white italic md:text-sm lg:text-base">
           {t('text.terms')}
         </h1>
-        <p className="font-futura text-center text-[10px] text-white italic md:text-sm lg:text-base">{t('text.termsText')}</p>
+        <p className="font-futura text-center text-[10px] text-white md:text-sm lg:text-base">
+          This website and the live production The Women Who Made Us contain excerpts of copyrighted
+          materials including audio, video, and still images used for purposes of education,
+          commentary, criticism, and tribute. Such use is believed to constitute fair use under 17
+          U.S.C. § 107 of the United States Copyright Act. Some materials are used in a de minimis
+          manner. All rights remain with their respective copyright holders.
+        </p>
       </div>
     </div>
   )
