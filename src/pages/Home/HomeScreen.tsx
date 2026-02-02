84a11caeb042889fe79f4edfb0e5ebb9324d3340
WilliamSalcedo <wsl_1994@hotmail.com>
2026-02-02 18:13:06 -0500

    Delete component header

--- src/pages/Home/HomeScreen.tsx
+++ src/pages/Home/HomeScreen.tsx
@@ -1,11 +1,11 @@
-import Header from '../../components/layout/Header'
 import Main from '../../components/layout/Main'
+import MusicDrawer from '../../components/MusicDrawer'
 
 export default function Home() {
   return (
-    <section className="mx-[10px] sm:mx-[20px] lg:mx-[30px] my-6">
-      <Header />
+    <section className="mx-[10px] my-6 sm:mx-[20px] lg:mx-[30px]">
       <Main />
+      <MusicDrawer />
     </section>
   )
 }
