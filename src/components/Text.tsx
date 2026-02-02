7249ac9315d9d1afd789d276b1312d92bc434a2e
WilliamSalcedo <wsl_1994@hotmail.com>
2026-02-02 18:06:35 -0500

    Change styles

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,4 +1,5 @@
 import { useTranslation, Trans } from "react-i18next";
+import subTitle from '../assets/LandingSubTitle.png'
 
 export default function Text() {
   const { t } = useTranslation();
@@ -6,15 +7,13 @@ export default function Text() {
   return (
     <div>
       <div className="flex justify-center">
-        <p className="text-center text-xs text-[#fda963] sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-futura">
+        <p className="text-center text-xs text-[#fda963] sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-OldAlmanac">
           <Trans i18nKey="text.intro" />
         </p>
       </div>
 
-      <div className="m-4 flex justify-center">
-        <p className="text-[#397269ff] font-futura max-w-xs text-center text-lg sm:max-w-md sm:text-xl md:max-w-2xl md:text-2xl lg:max-w-4xl lg:text-3xl xl:max-w-5xl xl:text-4xl 2xl:max-w-6xl 2xl:text-5xl">
-          {t("text.ctaTitle")}
-        </p>
+      <div className="m-8 flex justify-center">
+        <img src={subTitle} alt="" className="w-60 md:w-130 lg:w-140" />
       </div>
 
       <div className="m-4 flex justify-center">
