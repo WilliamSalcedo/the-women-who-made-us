a34ee1ac24182a363b4f2a1691cfd2bf18a23596
WilliamSalcedo <wsl_1994@hotmail.com>
2026-02-02 17:58:36 -0500

    Add a new routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,20 @@ import './App.css'
 import Home from './pages/Home/HomeScreen'
 import About from './pages/About/About'
 import NotFound from './pages/NotFound/NotFound'
+import PrivacyModal from './components/PrivacyModal'
+import TermsModal from './components/TermsModal'
+
 
 function App() {
   return (
     <>
+    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
+        <Route path='/PrivacyModal' element={<PrivacyModal />} />
+        <Route path='/TermsModal' element={<TermsModal />} />
       </Routes>
     </>
   )
