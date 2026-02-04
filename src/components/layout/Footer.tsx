3beb4fd9965042f33e459ea0669d85644aa5e677
WilliamSalcedo <wsl_1994@hotmail.com>
2026-02-04 17:33:47 -0500

    Update style in component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -10,10 +10,11 @@ export default function Footer() {
 
   return (
     <div>
-      <div className="font-futura mt-20 flex justify-center divide-x divide-white text-[9px] text-[#9b9999] md:text-sm lg:text-base">
+      <div className="font-futura mt-20 flex justify-center text-[9px] text-[#9b9999] md:text-sm lg:text-base">
         <button onClick={onHandlePrivacy} className="px-3 transition hover:opacity-75">
           {t('text.privacy')}
         </button>
+        <span className="h-6 w-px bg-white opacity-30" />
         <button onClick={onHandleTerms} className="px-3 transition hover:opacity-75">
           {t('text.terms')}
         </button>
