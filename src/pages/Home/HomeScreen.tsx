22bf2a68336a4871e86447aa312b3e7e1fe64774
WilliamSalcedo <wsl_1994@hotmail.com>
2026-02-04 17:34:15 -0500

    Delete component music

--- src/pages/Home/HomeScreen.tsx
+++ src/pages/Home/HomeScreen.tsx
@@ -1,11 +1,9 @@
 import Main from '../../components/layout/Main'
-import MusicDrawer from '../../components/MusicDrawer'
 
 export default function Home() {
   return (
     <section className="mx-[10px] my-6 sm:mx-[20px] lg:mx-[30px]">
       <Main />
-      <MusicDrawer />
     </section>
   )
 }
