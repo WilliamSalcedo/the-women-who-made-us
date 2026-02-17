894acd9841b59bc1ec168ac6aa7242cfe0a94458
WilliamSalcedo <wsl_1994@hotmail.com>
2026-02-16 20:58:08 -0500

    fix text in music drawer component

--- src/components/MusicDrawer.tsx
+++ src/components/MusicDrawer.tsx
@@ -1,9 +1,26 @@
-import music from '../assets/Music.png'
-
 export default function MusicDrawer() {
   return (
-    <nav className="flex items-center justify-center">
-      <img src={music} alt="music" className="w-80 md:w-150 lg:w-250" />
-    </nav>
+    <div>
+      <h1 className="font-futura mt-4 mb-2 text-center text-white italic md:text-[25px] lg:text-[30px]">
+        The Music
+      </h1>
+      <div className="flex justify-center text-white">
+        <p className="mt-10 w-[80%] text-justify">
+          From the moment you step into the venue, the music begins doing what it does best.
+          Bringing memories back, setting the mood, and connecting generations. HER is a fully
+          curated musical journey that celebrates the women who shaped us, from the early decades of
+          recorded music to the icons of today. <br /> The show travels through time and genres,
+          blending classic standards, theatrical ballads, pop anthems, country legends, dance floor
+          divas, and modern hits. Each song is selected with intention, designed to spark nostalgia,
+          emotion, and that instant recognition of “this made me who I am.” <br /> This is not just
+          a concert. It is a celebration of women in music, across eras, styles, and stories. A
+          soundtrack to our collective memory, and a love letter to the voices that raised us.{' '}
+          <br /> For the legal mini disclaimer area, you can add something like this in tiny footer
+          font: <br /> <b>Legal Notice</b> <br /> This production may include brief audiovisual
+          excerpts and archival materials for commentary, educational, and artistic purposes in
+          accordance with applicable fair use and copyright exception.
+        </p>
+      </div>
+    </div>
   )
 }
