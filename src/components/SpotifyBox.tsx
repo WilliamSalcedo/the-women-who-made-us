767b7e6a285c4f0d3afee2bc28f0dfd9648487a7
WilliamSalcedo <wsl_1994@hotmail.com>
2026-02-20 17:14:16 -0500

    Add a new component for spotify

---
+++ src/components/SpotifyBox.tsx
@@ -0,0 +1,16 @@
+interface SpotifyBoxProps {
+  linkSpotify: string
+}
+function SpotifyBox({ linkSpotify }: SpotifyBoxProps) {
+  return (
+    <iframe
+      src={linkSpotify}
+      loading="lazy"
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      allowFullScreen
+      className="h-[232px] w-full rounded-xl border-0 md:h-[200px] md:w-[80%]"
+    />
+  )
+}
+
+export default SpotifyBox
