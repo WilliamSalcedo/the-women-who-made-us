d4ffebd688621c80157666d4a39e91fd234a711c
WilliamSalcedo <wsl_1994@hotmail.com>
2026-03-02 16:39:25 -0500

    feat: add dynamic className support to ArchiveCards component

--- src/components/ArchiveCards.tsx
+++ src/components/ArchiveCards.tsx
@@ -2,12 +2,15 @@ interface ArchiveCardsProps{
   image:string
   title?:string
   description?:string
+  iframeClassName?: string
 }
 
-export default function ArchiveCards({image, title, description}: ArchiveCardsProps) {
+export default function ArchiveCards({image, title, description, iframeClassName}: ArchiveCardsProps) {
+
+  const defaultStyles = "mb-4 w-35"
   return (
     <div className="flex flex-col items-center p-4 text-center">
-      <img src={image} alt="" className="mb-4 w-35" />
+      <img src={image} alt="" className={iframeClassName ? iframeClassName : defaultStyles} />
       <h1 className="font-bold">{title}</h1>
       <p className="text-sm">
         {description}
