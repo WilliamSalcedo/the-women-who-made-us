df8cb387dcf022fac3974573d0b646ced16bb544
WilliamSalcedo <wsl_1994@hotmail.com>
2026-03-02 16:42:54 -0500

    Add componnts in main layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,9 @@
 
 import { Outlet } from 'react-router-dom'
 import Navbar from '../Navbar'
+import SocialMedia from '../SocialMedia'
+import Form from '../Form'
+import Footer from './Footer'
 
 export default function MainLayout() {
   return (
@@ -9,6 +12,9 @@ export default function MainLayout() {
       <main >
         <Outlet />
       </main>
+      <Form />
+      <SocialMedia />
+      <Footer />
     </>
   )
 }
